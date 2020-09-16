92653adce6503ca861f1d7ebd992f45934301402
Alfonso Tristan Jay Arias <AMArias@smart.com.ph>
2020-09-16 18:16:47 +0800

    fixed git issues

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,9 +1,4 @@
-import Vue from "vue";
-import axios from "axios";
+import Vue from 'vue'
+import axios from 'axios'
 
-const PROD = location.origin + "/ft-esa/";
-
-if (process.env.PROD) axios.defaults.baseURL = PROD;
-// axios.defaults.withCredentials = true;
-
-Vue.prototype.$axios = axios;
+Vue.prototype.$axios = axios
