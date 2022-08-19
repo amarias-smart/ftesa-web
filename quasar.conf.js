e282c4d49de1c2c2479ea2989ebc1c45203886d7
Alfonso Tristan Jay Arias <AMArias@smart.com.ph>
2022-08-19 14:44:14 +0800

    app update

--- quasar.conf.js
+++ quasar.conf.js
@@ -40,7 +40,7 @@ module.exports = function(/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: "history", // available values: 'hash', 'history'
-      publicPath: "/",
+      publicPath: "/ft-esa/",
 
       // transpile: false,
 
